7eeb72f7fe1e4eebe89a3c8c8e9b9ddbcac8cca9
RyoTN <13426723+ryotn@users.noreply.github.com>
2025-07-12 23:07:59 +0900

    fix: update open-graph-scraper and chardet versions; enhance OgImage to handle Amazon URLs

--- src/Utils/OgImage.js
+++ src/Utils/OgImage.js
@@ -8,7 +8,7 @@ class OgImage {
   static async getOgImage(url, ccClient = undefined) {
     try {
       const { ogImageUrl, title, description } = await this.getOgp(url, ccClient)
-      const ogImage = await this.getImage(ogImageUrl)
+      const ogImage = ogImageUrl ? await this.getImage(ogImageUrl) : undefined
 
       return {
         imageUrl: ogImageUrl,
@@ -66,12 +66,19 @@ class OgImage {
         console.error(e)
       }
     }
-    
+
     if (!ogImageUrl) { // ccClientがない場合や、ccClientのsummaryが取得できなかった場合は、OpenGraphを使う
       const { result } = await ogs({ url: url })
-      ogImageUrl = result.ogImage?.at(0)?.url ?? GOOGLE_FAVICON_URL + url
-      if (!title) title = result.ogTitle ?? ""
-      if (!description) description = result.ogDescription ?? ""
+      if (this.containsAmazonShortURL(url) && result.ogImage?.length > 0) {
+        // Amazonの短縮URLの場合、ogImageの中から特定のパターンを持つ画像を選ぶ
+        // https://zenn.dev/st43/scraps/f9940dbba495d3
+        const imageUrl = this.findTargetAmazonImage(result)
+        ogImageUrl = imageUrl ? imageUrl : ""
+      } else {
+        ogImageUrl = result.ogImage?.at(0)?.url ?? GOOGLE_FAVICON_URL + url
+      }
+      if (result.ogTitle) title = result.ogTitle
+      if (result.ogDescription) description = result.ogDescription
     }
 
     return {
@@ -79,6 +86,43 @@ class OgImage {
       title: title,
       description: description,
     }
+  }
+
+  static containsAmazonShortURL(text) {
+    const pattern = /https?:\/\/(?:a\.co|amzn\.to|amzn\.asia|amzn\.eu|(?:www\.)?amazon\.co\.jp)\/[^\s]+/i
+    return pattern.test(text)
+  }
+
+  static findTargetAmazonImage(json) {
+    const prefix = "https://m.media-amazon.com/images/I/"
+
+    // 条件に合うURLを探す
+    const image = json.ogImage.find(item =>
+      item.url &&
+      item.url.startsWith(prefix) &&
+      item.url.includes("_SX") &&
+      item.url.includes("_SY")
+    )
+
+    if (!image) return undefined
+
+    const originalUrl = image.url
+
+    // originalUrl例: 
+    // https://m.media-amazon.com/images/I/51Di4bc19jL.__AC_SX300_SY300_QL70_ML2_.jpg
+
+    // 画像IDを取り出す（prefixの後ろと、ドット（.）より前まで）
+    // 例: "51Di4bc19jL"
+    const idMatch = originalUrl.match(/https:\/\/m\.media-amazon\.com\/images\/I\/([^\.]+)\./)
+
+    if (!idMatch || !idMatch[1]) return undefined
+
+    const imageId = idMatch[1]
+
+    // 新しいURLフォーマットに埋め込む
+    const newUrl = `https://m.media-amazon.com/images/I/${imageId}.jpg_BO30,255,255,255_UF900,850_SR1910,1000,0,AmazonEmber,50,4,0,0_QL100_.jpg`
+
+    return newUrl
   }
 }
 
